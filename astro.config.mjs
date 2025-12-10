8648f1e84f185a0734b5a0de5ff29a431b2f0f26
llingqing <2705823089@qq.com>
2025-12-10 22:28:49 +0800

    fix1

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import mdx from "@astrojs/mdx";
 import searchIndexer from "./src/integrations/searchIndex.mts";
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lingqing.netlify.app',
+	site: 'https://llingqing.github.io',
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -8,7 +8,7 @@ const SITE_LANG = "zh_CN";
 export const siteConfig: SiteConfig = {
 	title: "lingluoa",
 	subtitle: "个人博客",
-	site_url: "https://lingqing.netlify.app/",
+	site_url: "https://llingqing.github.io",
 	description:
 		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
 	keywords: [
