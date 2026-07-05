7fb8778012aa06921a94c31f766cd87a65081af6
llingqing <2705823089@qq.com>
2026-07-05 14:16:01 +0800

    add stack-applications

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://weavatar.com/avatar/e3cdc91b78e964662d8c56eb1673de882ad29a25c3853b7168afd2adc9ae7faa",
 
 	// 名字
 	name: "lingluoa",
